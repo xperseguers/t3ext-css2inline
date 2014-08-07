5c5fbcf290a23fc8eaf208d61a60705c3c610492
Xavier Perseguers <xavier@causal.ch>
2014-08-07 14:30:21 +0200

    [TASK] Update TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'description' => 'Moves the styles from CSS sheets into inline CSS, in order to comply with uncooperative email clients. Can be used as a post-processing stdWrap in TypoScript.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.1.5',
+	'version' => '0.1.6-dev',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -32,22 +32,16 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
+	'constraints' => array(
+		'depends' =>  array(
+			'typo3' => '4.5.0-6.2.99',
 		),
-		'conflicts' => 
-		array (
+		'conflicts' => array(
 		),
-		'suggests' => 
-		array (
+		'suggests' =>  array(
 		),
 	),
 	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"89e7";s:12:"ext_icon.gif";s:4:"ef6a";s:17:"ext_localconf.php";s:4:"3601";s:10:"README.txt";s:4:"ee2d";s:14:"doc/manual.sxw";s:4:"ecaa";s:19:"doc/wizard_form.dat";s:4:"3ca4";s:20:"doc/wizard_form.html";s:4:"9b71";s:31:"pi1/class.tx_css2inline_pi1.php";s:4:"176c";}',
-	'suggests' => 
-	array (
-	),
 );
 
 ?>
