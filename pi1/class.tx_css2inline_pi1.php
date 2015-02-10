a9d4dd3b2f545ab0b830da96ef7e436090ea3a7a
Patrick Schriner <patrick.schriner@diemedialen.de>
2015-02-10 12:21:43 +0100

    Suppress XML parser warnings

--- pi1/class.tx_css2inline_pi1.php
+++ pi1/class.tx_css2inline_pi1.php
@@ -107,7 +107,9 @@ class tx_css2inline_pi1 extends tslib_pibase {
 		$xmldoc->strictErrorChecking = false;
 		$xmldoc->formatOutput = true;
 		$xmldoc->encoding = $this->encoding;
+		$errors = libxml_use_internal_errors(true);
 		$xmldoc->loadHTML($body);
+		libxml_use_internal_errors($errors);
 		$xmldoc->normalizeDocument();
 
 		$xpath = new DOMXPath($xmldoc);
